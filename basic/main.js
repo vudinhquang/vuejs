e66e64dd2c4a13234a745b8e1824c207f9c4fe56
vudinhquang <vudinhquangk53@gmail.com>
2019-09-14 01:17:45 +0900

    Phân tích data và Xử lí Price

--- basic/main.js
+++ basic/main.js
@@ -4,12 +4,39 @@ var vueInstance = new Vue({
         title : 'Áo thun nam thể thao hàng VNXK vải dày mịn - Vải Đốm',
         url   : 'https://www.lazada.vn/products/ao-thun-nam-the-thao-hang-vnxk-vai-day-min-vai-dom-i265780948-s382816279.html',
         target: '_blank',
-        price : '12000',
-        check : true
+        price : '20000',
+        // check : true
+        sale: 0.1,
+        selectedProduct: 0,
+        listProductDetail: [
+            {
+                image: './images/blue.jpg',
+                quantity: 8,
+                textColor: 'Màu Xanh'
+            },
+            {
+                image: './images/black.jpg',
+                quantity: 2,
+                textColor: 'Màu Đen'
+            },
+            {
+                image: './images/red.jpg',
+                quantity: 0,
+                textColor: 'Màu Đỏ'
+            },
+        ]
     },
     methods: {
-        formatPrice() {
+
+    },
+    computed: {
+        formatOriginalPrice() {
             var number = this.price;
+
+            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number);
+        },
+        formatFinalPrice() {
+            var number = this.price - this.sale * this.price;
 
             return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number);
         }
