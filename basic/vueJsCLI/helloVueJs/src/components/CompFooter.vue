7d398efd2eb72b848c657728d1bbeb93d63f01fe
vudinhquang <vudinhquangk53@gmail.com>
2019-09-15 14:29:36 +0900

    Tìm hiểu Component và cách tạo trong Vue

---
+++ basic/vueJsCLI/helloVueJs/src/components/CompFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer><h1>{{ textFooter }}</h1></footer>
+</template>
+
+<script>
+export default {
+    name: 'comp-footer',
+    data () {
+        return {
+            textFooter: 'Hello Footer'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
