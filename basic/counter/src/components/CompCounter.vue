4ded2a063d2e25785b1a92b895461d6f10a9bd2c
quang <vudinhquangk53@gmail.com>
2019-09-25 10:53:03 +0900

    state

---
+++ basic/counter/src/components/CompCounter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ count }}</h1>
+        <button v-on:click="count++">Plus</button>
+        <button v-on:click="count--">Minus</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'comp-counter',
+    data() {
+        return {
+         
+        }
+    },
+    computed: {
+        count () {
+            return this.$store.state.count
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ basic/counter/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './App.vue'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  }
+})
+
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
+
+
+// Mỗi thành phần trong Web tương ứng mỗi Component -> Tương ứng với 1 file *.vue
