e900f5777505d36a43f06a9037c7ea264706182f
vudinhquang <vudinhquangk53@gmail.com>
2019-09-29 16:10:51 +0900

    Cài đặt mã nguồn, dựa vào index.html ban đầu chia thành các Component con

---
+++ shoppingCart/shopping_cart/src/components/CartBodyTable.vue
@@ -0,0 +1,23 @@
+<template>
+    <tbody id="my-cart-body">
+        <cart-body-item />
+        <cart-body-item />
+        <cart-body-item />
+        <cart-body-item />
+    </tbody>
+</template>
+
+<script>
+import CartBodyItem from './CartBodyItem';
+
+export default {
+    name: 'cart-body-table',
+    components: {
+        CartBodyItem
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ shoppingCart/shopping_cart/src/components/CartWrapper.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h1 class="panel-title">Your Cart</h1>
+            </div>
+            <div class="panel-body">
+                <table class="table">
+                    <cart-header />
+                    
+                    <cart-body-table />
+
+                    <cart-footer />
+                </table>
+            </div>
+        </div>
+        <notify />
+    </div>
+</template>
+
+<script>
+import CartBodyTable from './CartBodyTable';
+import CartHeader from './CartHeader';
+import CartFooter from './CartFooter';
+import Notify from './Notify';
+export default {
+    name: 'cart-wrapper',
+    components: {
+        CartBodyTable,
+        CartHeader,
+        CartFooter,
+        Notify
+    }
+}
+</script>
+
+<style>
+
+</style>
