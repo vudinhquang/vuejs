d9a2e05052144b494af27279558452fc27fbda89
quang <vudinhquangk53@gmail.com>
2019-09-30 09:53:16 +0900

    Xây dựng chức năng Add sản phẩm vào giỏ hàng - State & Empty

--- shoppingCart/shopping_cart/src/components/CartBodyTable.vue
+++ shoppingCart/shopping_cart/src/components/CartBodyTable.vue
@@ -1,19 +1,32 @@
 <template>
     <tbody id="my-cart-body">
-        <cart-body-item />
-        <cart-body-item />
-        <cart-body-item />
-        <cart-body-item />
+        <template v-if="items.length">
+            <cart-body-item v-for="(item, index) in items" :key="index"/>
+        </template>
+
+        <tr v-else><th colspan="6">{{ NOTI_EMPTY_PRODUCT }}</th></tr>
     </tbody>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { NOTI_EMPTY_PRODUCT } from '../constants/config'
 import CartBodyItem from './CartBodyItem';
 
 export default {
     name: 'cart-body-table',
+    data() {
+        return {
+            NOTI_EMPTY_PRODUCT
+        }
+    },
     components: {
         CartBodyItem
+    },
+    computed: {
+        ...mapState({
+            items: state => state.cart.listCarts
+        })
     }
 }
 </script>
---
+++ shoppingCart/shopping_cart/src/constants/config.js
@@ -0,0 +1,5 @@
+const NOTI_EMPTY_PRODUCT = 'Empty product in your cart'
+
+export {
+    NOTI_EMPTY_PRODUCT
+}
