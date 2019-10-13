fe42b93eede6bd8cfbec605b7695c6a35c111166
vudinhquang <vudinhquangk53@gmail.com>
2019-10-13 23:29:00 +0900

    Install Vue Router

--- vue_router/src/main.js
+++ vue_router/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import './plugins'
+
+Vue.use(VueRouter)
+
+const routes = [
+
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
