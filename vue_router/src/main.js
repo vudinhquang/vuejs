c27e9423fdaa21133bfcd535932a4cc720bcf51a
vudinhquang <vudinhquangk53@gmail.com>
2019-10-14 16:35:53 +0900

    Router View

--- vue_router/src/main.js
+++ vue_router/src/main.js
@@ -1,21 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import Home from './pages/Home.vue'
+import AboutUs from './pages/AboutUs.vue'
 
 import './plugins'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+	{ path: '/', component: Home },
+	{ path: '/about-us', component: AboutUs },
 ]
 
 const router = new VueRouter({
-  routes // short for `routes: routes`
+	routes // short for `routes: routes`
 })
 
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+	el: '#app',
+	router,
+	render: h => h(App)
 })
---
+++ vue_router/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-container>
+            <b-jumbotron header="HomePage">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Natus quia commodi culpa quam id repellat expedita fugit adipisci totam, 
+                    harum quibusdam alias consectetur debitis, quaerat odit quidem quae praesentium. Doloribus?
+                </p>
+                <b-button variant="primary" href="#">More Info</b-button>
+            </b-jumbotron>
+        </b-container>
+    </div>
+</template>
+<script>
+export default {
+    name: 'home'
+}
+</script>
+
+<style>
+
+</style>
