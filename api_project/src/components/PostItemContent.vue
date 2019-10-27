3ed2058c719577d7c917c0d9710c8349aeb23232
vudinhquang <vudinhquangk53@gmail.com>
2019-10-27 15:11:47 +0900

    Post Item Content

--- api_project/src/components/PostItemContent.vue
+++ api_project/src/components/PostItemContent.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="ass1-section__content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas voluptates magnam, odit quae aut vel 
-            accusantium. Alias pariatur quidem, minus eaque officiis, sed ea repellendus tenetur ipsa inventore 
-            est earum.
-        </p>
+    <div class="ass1-section__content" v-if="post">
+        <p>{{ post.post_content }}</p>
         <div class="ass1-section__image">
-            <a href="#"><img src="/dist/images/microphone-1209816_1920.jpg" alt=""></a>
+            <router-link v-bind:to="{ name: 'post-detail', params: { id: post.PID } }">
+                <img v-bind:src="post.url_image" v-bind:alt="post.post_content">
+            </router-link >
         </div>
     </div>
 </template>
