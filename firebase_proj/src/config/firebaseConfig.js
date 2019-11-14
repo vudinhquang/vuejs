ea1a61e5db9ca208d970a2912f74db4bc44679ff
quang <vudinhquangk53@gmail.com>
2019-11-14 12:08:22 +0900

    Install Firbase

---
+++ firebase_proj/src/config/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxsjbi8nauxxj9Tll4M6HJe0JoTYwb0qg",
+    authDomain: "vuejs-9b86e.firebaseapp.com",
+    databaseURL: "https://vuejs-9b86e.firebaseio.com",
+    projectId: "vuejs-9b86e",
+    storageBucket: "vuejs-9b86e.appspot.com",
+    messagingSenderId: "271018060056",
+    appId: "1:271018060056:web:2ec7f991423e5d2292166d",
+    measurementId: "G-8DJ85SPRVC"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
