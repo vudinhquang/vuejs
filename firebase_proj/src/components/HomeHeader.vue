8635e9797c6aab0fdeceb515e1ae174af3e159cf
quang <vudinhquangk53@gmail.com>
2019-11-18 10:36:49 +0900

    Keep Login

--- firebase_proj/src/components/HomeHeader.vue
+++ firebase_proj/src/components/HomeHeader.vue
@@ -6,19 +6,31 @@
                 <li><router-link to="/create">Tạo Task</router-link></li>
             </ul>
             <ul class="right">
-                <li>Đăng nhập với <a href="/">example@gmail.com</a></li>
-                <li><a href="/">Đăng xuất</a></li>
+                <li v-if="currentUser.email">Đăng nhập với <router-link to="/">{{ currentUser.email }}</router-link></li>
+                <li><a href="/" v-on:click.prevent="handleLogout">Đăng xuất</a></li>
             </ul>
         </div>
     </header>
 </template>
 
 <script>
+import { auth } from '../config/firebase'
+import { mapState } from 'vuex'
 export default {
     name: 'home-header',
     data() {
         return {
 
+        }
+    },
+    computed: {
+        ...mapState(['currentUser'])
+    },
+    methods: {
+        handleLogout() {
+            if (window.confirm('Bạn có chắc chắn muốn đăng xuất?')) {
+                auth.signOut();
+            }
         }
     }
 }
--- firebase_proj/src/main.js
+++ firebase_proj/src/main.js
@@ -5,8 +5,22 @@ import store from './store'
 import Datetime from 'vue-datetime'
 import 'vue-datetime/dist/vue-datetime.css'
 import './assets/style.css'
+import { auth } from './config/firebase'
+
 
 Vue.use(Datetime)
+
+auth.onAuthStateChanged(function(user) {
+  if (user) {
+    let data = { email: user.email, uid: user.uid };
+    store.commit('set_current_user', data);
+    router.push('/');
+  } else {
+    let data = { email: '', uid: '' };
+    store.commit('set_current_user', data);
+    router.push('/login');
+  }
+});
 
 new Vue({
   el: '#app',
